21d592a678a55871af7d22d8949b02603e52a852
max <max.assis@chatguru.com.br>
2024-06-13 19:47:35 -0300

    feat: fazendo o endpoint de pagar dados do usuario

---
+++ src/modules/Users/useCases/getUserData.usecase.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infra/database/prisma.service';
+
+@Injectable()
+export class GetUserDataUseCase {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUserData(id: string, name: string) {
+    const user = await this.prisma.userData.findUnique({
+      where: {
+        usersId: id,
+      },
+    });
+
+    return {
+      ...user,
+      name,
+    };
+  }
+}
