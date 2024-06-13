1558f43419d4aed174abbf8f6159722be3e9afb4
max <max.assis@chatguru.com.br>
2024-06-13 00:57:10 -0300

    feat: fazendo o relacionamento de configuraçoes de usuario

--- src/modules/Users/useCases/saveAvatar.usecase.ts
+++ src/modules/Users/useCases/saveAvatar.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { extname } from 'path';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { Supabase } from 'src/infra/providers/storage/storage-supabase';
@@ -14,13 +14,39 @@ export class UploadAvatarUseCase {
     const extFile = extname(file.originalname);
     const newName = `${id}${extFile}`;
 
-    const data = await this.supabase.client.storage
+    if (!file.mimetype.startsWith('image/')) {
+      throw new HttpException(
+        'the file is not a image',
+        HttpStatus.BAD_GATEWAY,
+      );
+    }
+
+    const fileUpload = await this.supabase.client.storage
       .from(process.env.SUPABASE_BUCKET!)
       .upload(newName, file.buffer, {
         upsert: true,
         contentType: file.mimetype,
       });
 
-    return data;
+    if (fileUpload.error) {
+      throw new HttpException("Can't upload file", HttpStatus.BAD_REQUEST);
+    }
+
+    const user = await this.prisma.userData.update({
+      where: {
+        usersId: id,
+      },
+      data: {
+        avatar:
+          'https://iijythvtsrfruihwseua.supabase.co/storage/v1/object/public/avatars/' +
+          fileUpload.data.path,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException("Can't upload file", HttpStatus.BAD_REQUEST);
+    }
+
+    return { avatar_url: user.avatar };
   }
 }
