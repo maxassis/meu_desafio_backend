2abdcb376436626afe1212941eba4913ec68d37b
max <max.assis@chatguru.com.br>
2024-06-23 09:56:21 -0300

    feat: corrigido endpoint de verificação de email

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -36,6 +36,8 @@ export class LoginController {
   @Post('/checkEmail')
   async checkEmail(@Body() data: CheckEmailDTO) {
     const { email } = data;
+    console.log('EMAIL, email')
+
     return this.checkValidEmailUseCase.checkEmail(email);
   }
 
