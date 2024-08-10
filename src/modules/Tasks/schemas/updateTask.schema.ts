352b3323f8d5ea40857067b736f52567b79e85d9
max <max.assis@chatguru.com.br>
2024-08-10 20:11:47 -0300

    feat: ajustes na tipagem

--- src/modules/Tasks/schemas/updateTask.schema.ts
+++ src/modules/Tasks/schemas/updateTask.schema.ts
@@ -1,10 +1,22 @@
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'nestjs-zod/z';
 
+const isoDateTimeUtcRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+
 const UpdateTaskSchema = z.object({
   name: z.string().optional(),
   environment: z.enum(['livre', 'esteira']),
-  date: z.date().optional(),
+  date: z
+    .string()
+    .optional()
+    .refine(
+      (value) => {
+        return value ? isoDateTimeUtcRegex.test(value) : true;
+      },
+      {
+        message: 'Invalid date format. Expected YYYY-MM-DDTHH:MM:SSZ.',
+      },
+    ),
   duration: z.string().optional(),
   calories: z.number().optional(),
   distanceKm: z.number(),
