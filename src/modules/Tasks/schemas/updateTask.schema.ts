21ce23b404d23ba3b4abaca92eac832ba890f185
max <max.assis@chatguru.com.br>
2025-05-23 12:19:07 -0300

    feat: corrigido schema de data para updateTask

--- src/modules/Tasks/schemas/updateTask.schema.ts
+++ src/modules/Tasks/schemas/updateTask.schema.ts
@@ -1,22 +1,13 @@
 import { createZodDto } from '@anatine/zod-nestjs';
 import { z } from 'zod';
 
-const isoDateTimeUtcRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
 
 const UpdateTaskSchema = z.object({
   name: z.string().optional(),
   environment: z.enum(['livre', 'esteira']),
-  date: z
-    .string()
-    .optional()
-    .refine(
-      (value) => {
-        return value ? isoDateTimeUtcRegex.test(value) : true;
-      },
-      {
-        message: 'Invalid date format. Expected YYYY-MM-DDTHH:MM:SSZ.',
-      },
-    ),
+  date: z.string().datetime({
+    message: 'Invalid date format. Expected YYYY-MM-DDTHH:MM:SSZ.',
+  }),
   duration: z
     .number({ message: 'duration Required' })
     .positive({ message: 'duration must be greater than zero' }),
