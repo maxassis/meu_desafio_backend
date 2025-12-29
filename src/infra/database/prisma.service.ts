5d3757d323949a904deb6f9d2bc3e2975262b9f2
maxassis <max.assis@ymail.com>
2025-12-29 18:01:44 -0300

    install prisma 7

--- src/infra/database/prisma.service.ts
+++ src/infra/database/prisma.service.ts
@@ -1,8 +1,23 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Env } from 'src/env';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private configService: ConfigService<Env, true>) {
+    const connectionString = configService.get('DATABASE_URL', { infer: true });
+    super({
+      adapter: new PrismaPg({
+        connectionString,
+      }),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
